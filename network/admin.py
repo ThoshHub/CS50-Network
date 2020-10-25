3ade8ea8e49da2af1c778eba18f85feb8129c4ce
ThoshHub <Shamthosh@gmail.com>
2020-10-24 23:58:58 -0400

    user attributes now have followers and following list, they both show up in the admin console

--- network/admin.py
+++ network/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
+
+class UserAdmin(admin.ModelAdmin): 
+	model = User
+admin.site.register(User, UserAdmin) # Custom fields don't show up if you register UserAdmin... (?)
+# admin.site.register(User) # OR just use this instead of the above 3 lines
--- network/models.py
+++ network/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib import admin
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+	followers = models.ManyToManyField("User", related_name="followedby")
+	following = models.ManyToManyField("User", related_name="follows")
+	# temp = models.CharField(max_length=64)
