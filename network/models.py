4e569e392ddbcb87b6ee0895cad1acd3c70a1868
ThoshHub <Shamthosh@gmail.com>
2020-10-25 17:48:38 -0400

    updated messages model

--- network/models.py
+++ network/models.py
@@ -7,3 +7,6 @@ class User(AbstractUser):
 	followers = models.ManyToManyField("User", related_name="followedby")
 	following = models.ManyToManyField("User", related_name="follows")
 	# temp = models.CharField(max_length=64)
+
+class message(models.Model):
+	content = models.TextField # not limited to 140 chars, but no requirement existed in the assignment
