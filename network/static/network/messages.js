4d19ac46fad19e6da5d99755961522b190266e85
ThoshHub <Shamthosh@gmail.com>
2020-10-28 23:50:46 -0400

    Added event listener for submit button (there's only one on the index page)

--- network/static/network/messages.js
+++ network/static/network/messages.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 	console.log("page loaded!")
+
+	document.addEventListener('click', event => {
+	  const element = event.target;
+	  console.log("Something was clicked")
+	  // need to test whether the element's id = "submit_message_button"
+	  // if it is, then send fetch request to create object in database
+	  // after that next step is to start displaying posts (maybe play with animations?)
+  })
 });
