f12b7da9a83e59c912f5730bce7a2d655492837e
ThoshHub <shamthosh@gmail.com>
2020-10-29 23:39:30 -0400

    need to create message object and save it

--- network/static/network/messages.js
+++ network/static/network/messages.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-	console.log("page loaded!")
+	// console.log("page loaded!")
 
-	document.addEventListener('click', event => {
-	  const element = event.target;
-	  console.log("Something was clicked")
-	  // need to test whether the element's id = "submit_message_button"
-	  // if it is, then send fetch request to create object in database
-	  // after that next step is to start displaying posts (maybe play with animations?)
+	document.addEventListener('click', event => { //unused as of 20.10.29
+		const element = event.target;
+		// console.log("Something was clicked")
+
+		if (element.id == "submit_message_button") {
+			submit_message()
+			
+		}
+		// need to test whether the element's id = "submit_message_button"
+		// if it is, then send fetch request to create object in database
+		// after that next step is to start displaying posts (maybe play with animations?)
+
+		// new plan looks like we CAN't do that because django doesn't let us
+		// if you go to the index.html page, the django form is submitted and the page is reloaded 
+		// automatically so it HAS to be through django, no problem though
   })
 });
+
+function submit_message() { //unused as of 20.10.29
+	console.log("message was submitted!")
+}
--- network/views.py
+++ network/views.py
@@ -6,60 +6,65 @@ from django.urls import reverse
 from .forms import messageForm
 from .models import User
 
-
 def index(request):
-    return render(request, "network/index.html", {
-		"form": messageForm()
-	})
-
+	# TODO need to show the user some sort of blank page if the user 
+	if request.method == "POST":
+		form = messageForm(request.POST)
+		if form.is_valid:
+			return_message = form.cleaned_data["message"] # get the message that the user submitted
+			# create a message object
+				# TODO
+			# save the message object
+				# TODO
+	return render(request, "network/index.html", {"form": messageForm()})
 
 def login_view(request):
-    if request.method == "POST":
+	if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+		# Attempt to sign user in
+		username = request.POST["username"]
+		password = request.POST["password"]
+		user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "network/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "network/login.html")
+		# Check if authentication successful
+		if user is not None:
+			login(request, user)
+			return HttpResponseRedirect(reverse("index"))
+		else:
+			return render(request, "network/login.html", {
+				"message": "Invalid username and/or password."
+			})
+	else:
+		return render(request, "network/login.html")
 
 
 def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+	logout(request)
+	return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+	if request.method == "POST":
+		username = request.POST["username"]
+		email = request.POST["email"]
 
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "network/register.html", {
-                "message": "Passwords must match."
-            })
+		# Ensure password matches confirmation
+		password = request.POST["password"]
+		confirmation = request.POST["confirmation"]
+		if password != confirmation:
+			return render(request, "network/register.html", {
+				"message": "Passwords must match."
+			})
 
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "network/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "network/register.html")
+		# Attempt to create new user
+		try:
+			user = User.objects.create_user(username, email, password)
+			user.save()
+		except IntegrityError:
+			return render(request, "network/register.html", {
+				"message": "Username already taken."
+			})
+		login(request, user)
+		return HttpResponseRedirect(reverse("index"))
+	else:
+		return render(request, "network/register.html")
