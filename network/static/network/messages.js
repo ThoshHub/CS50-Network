585bec4454c9540d15003595f7bb4a6978fccdd1
ThoshHub <shamthosh@gmail.com>
2020-11-29 20:05:53 -0500

    removed extraneous function

--- network/static/network/messages.js
+++ network/static/network/messages.js
@@ -28,74 +28,6 @@ function load() {
 	});
 }
 
-// DELETE THIS
-function get_user_1(id) {
-	var returnVal = ""
-	fetch('/users/' + id)
-	.then(res => res.json())
-	.then(data => {
-		// console.log(data);
-		// console.log(data[0]);
-		// console.log(data[0].name); // returns name
-		// console.log(data[0].name.toString()); // returns name
-		// console.log(typeof(data[0].name)); // returns string
-		returnVal = data[0].name.toString()
-	})
-	console.log("returnVal: ")
-	console.log(returnVal)
-	return returnVal;
-	// TODO returnVal is blank even though it is set in the .then statement
-}
-
-// DELETE THIS
-function get_user_2(id) {
-	var data;
-    // or $.get(...).then, or request(...).then, or query(...).then
-    fetch('/users/' + id).then(function(response){
-		data = response.json();
-	});
-	console.log(data)
-    return data;
-}
-
-// DELETE THIS
-function get_user_3(id) {
-    var data;
-    var data2;
-    // or $.get(...).then, or request(...).then, or query(...).then
-    fetch('/users/' + id).then(function(response){
-        data = response.json();
-        data.then(function(result){
-            // console.log(result[0].name)
-			data2 = result[0].name
-			return data2;
-        });
-    });
-    //console.log(data2) // THIS Doesn't Work, I need to review JS Promises
-    //return data;
-}
-
-// DELETE THIS
-function get_user_4(id) {
-    // RETURN the promise
-    return fetch('/users/' + id).then(function(response){
-		data = response.json();
-    	return data // process it inside the `then`
-	});
-}
-
-// DELETE THIS
-async function get_user_5(id) {
-	fetch('/users/' + id)
-	.then(res => res.json())
-	.then(data => {
-		returnVal = data[0].name.toString()
-	})
-	console.log("returnVal: ")
-	console.log(returnVal)
-	return returnVal; // returnVal is blank even though it is set in the .then statement
-}
-
 async function get_user(id) {
     const res = await fetch('/users/' + id);
     const data = await res.json();
@@ -135,22 +67,6 @@ async function display_message(element) {
 	document.getElementById(post.id).style.marginBottom = "10px";
 
 	// Make link clickable
-}
-
-// DELETE THIS
-function get_emails(mailbox){
-	fetch('/emails/' + mailbox)
-	.then(response => response.json())
-	.then(emails => {
-		// Print emails
-		console.log(emails);
-		
-		// ... do something else with emails ...
-		emails.forEach(element => {
-			console.log(mailbox)
-			create_email_listing(element, mailbox) // Mailbox is passed in because the individual email view depends on what mailbox the mail is from in display_email()
-		});
-	});
 }
 
 function submit_message() { //unused as of 20.10.29
