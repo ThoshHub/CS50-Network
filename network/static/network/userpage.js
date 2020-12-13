688a1a2ddd6a1454e637c85fbaeb7ffb728cf703
ThoshHub <shamthosh@gmail.com>
2020-12-13 16:41:25 -0500

    can return name from userpage

--- network/static/network/userpage.js
+++ network/static/network/userpage.js
@@ -8,6 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
 	})
 });
 
-function loadUserData(){
-    // need to call api here
+async function loadUserData(){
+	// need to call api here
+	const userData = await get_user(1);
+	console.log(userData);
+}
+
+async function get_user(id) {
+    const res = await fetch('/users/' + id);
+    const data = await res.json();
+    return data[0].name.toString();
 }
