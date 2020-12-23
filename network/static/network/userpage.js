8e3e70750835d4661e96f0d25ea8012122721c02
ThoshHub <shamthosh@gmail.com>
2020-12-23 02:13:50 -0500

    follow and unfollow now works!

--- network/static/network/userpage.js
+++ network/static/network/userpage.js
@@ -195,8 +195,7 @@ async function followUser(current_id, user_id){
 		console.log(data);
         
         // after following, clear the current follow button and add unfollow button
-        // document.getElementById('#follow_option').innerHTML = ""; 
-        // document.querySelector('#follow_option').innerHTML = "";
+        document.getElementById('follow_option').innerHTML = ""; 
         loadFollowStatus(current_id, user_id, "yes") // will now display unfollow button
     });
 }
@@ -213,9 +212,8 @@ async function unfollowUser(current_id, user_id){
 		console.log(data);
     
         // after following, clear the current follow button and add unfollow button
-        // document.getElementById('#follow_option').innerHTML = ""; 
-        // document.querySelector('#follow_option').innerHTML = "";
-        loadFollowStatus(current_id. user_id, "no") // will now display follow button
+        document.getElementById('follow_option').innerHTML = "";
+        loadFollowStatus(current_id, user_id, "no") // will now display follow button
     });
 }
 
