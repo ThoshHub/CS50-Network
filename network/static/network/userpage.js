72432846450eeeef2ca3e1e69c6592aa49d2f262
ThoshHub <shamthosh@gmail.com>
2021-01-03 04:31:40 -0500

    userpage can now like and dislike posts (but not edits yet)

--- network/static/network/userpage.js
+++ network/static/network/userpage.js
@@ -111,7 +111,14 @@ async function display_message(element, current_id) {
 	const writer_id = element.fields.writer;
 	const writer = capitalizeFirstLetter(element.fields.writername);
 	// const writer = "1";
-    const date = element.fields.date; // TODO Format this date
+	const date = element.fields.date; // TODO Format this date
+	
+	const numoflikes = element.fields.numoflikes.toString(); 
+	const liked_by = element.fields.liked_by;
+	const curr_user_likes_post = liked_by.includes(current_id);
+	// console.log("Number of Likes:\t" + numoflikes + "\nLiked By:\t" + liked_by);
+	// console.log("User ID: " + current_id + " Likes Message ID: " + element.pk + ": " + curr_user_likes_post.toString())
+
     const formatted_date = formatDate(date)
 	// console.log("Content: " + content.toString() + ", Writer: " + writer + ", Formatted Date: " + formatted_date)
 
@@ -121,7 +128,35 @@ async function display_message(element, current_id) {
 
 	// url is a placeholder
 	var html_str = "<h4>" + content + "</h4>" + "\n" + "<a href=/userpage/" + writer_id + ">" + writer + "</a>" + "<br>\n" + date;
-    // console.log(html_str)
+	
+	// Add number of likes and like/dislike button inside a new div
+	html_str += "<br><div id=like_div_" + element.pk + ">"
+	html_str += "<h6 id=numoflikes_" + element.pk + ">Likes: " + numoflikes + "</h6>";
+	
+	if(curr_user_likes_post){ // If the user likes the post, display the unlike button
+		const onclick_unlike = " onclick=\"unlike_post('" + element.pk + "','" + current_id+ "')\"" 
+		html_str += "<br>";
+		html_str += "<button div=unlike_button_" + element.pk; 
+		html_str += " type=\"button\"";
+		html_str += " class=\"btn btn-outline-danger\""
+		html_str += onclick_unlike;
+		html_str += ">";
+		html_str += "Unlike 💔"
+		html_str +=  "</button>"
+	} else { // if the user doesn't already like the post, display the like button
+		const onclick_like = " onclick=\"like_post('" + element.pk + "','" + current_id+ "')\"" 
+		html_str += "<br>";
+		html_str += "<button div=like_button_" + element.pk;
+		html_str += " type=\"button\"";
+		html_str += " class=\"btn btn-outline-primary\""
+		html_str += onclick_like;
+		html_str += ">";
+		html_str += "Like 💗"
+		html_str +=  "</button>"
+	}
+
+	html_str += "</div>"	
+
 	// Add Button to edit if it is the currently logged in user's own post
 	if(writer_id == current_id){
 		const onclick_str = " onclick=\"edit_post('" + element.pk + "','" + content+ "')\"" // calling the proper editing function
@@ -147,6 +182,73 @@ async function display_message(element, current_id) {
 	document.getElementById(post.id).style.borderRadius = "15px";
 	document.getElementById(post.id).style.padding = "10px";
 	document.getElementById(post.id).style.marginBottom = "10px";
+}
+
+async function like_post(message_id, current_id){
+	// console.log("User ID: " + current_id + " Liked Post: " + message_id + "!");
+	const res = await fetch('/message/like/' + message_id + '/' + current_id);
+    const data = await res.json();
+	// console.log(data);
+
+	reset_unlike_div(message_id, current_id); // pressing the like button creates the unlike button
+}
+
+async function unlike_post(message_id, current_id){
+	// console.log("User ID: " + current_id + " Unliked Post: " + post_id + "!");
+	const res = await fetch('/message/unlike/' + message_id + '/' + current_id);
+    const data = await res.json();
+	// console.log(data);
+
+	reset_like_div(message_id, current_id); // pressing the unlike button creates the like button
+}
+
+function reset_unlike_div(message_id, current_id){ // pressing the like button creates the unlike button
+	// console.log("resetting the div for: " + message_id.toString())
+	const div_id = "like_div_" + message_id.toString() // id for the edit div
+	const numoflikes_id = "numoflikes_" + message_id.toString() // id for the "Likes" label
+
+	const numoflikes_str = document.getElementById(numoflikes_id).innerText.charAt(7); //number of likes before user liked post
+	var new_numoflikes_str = parseInt(numoflikes_str) + 1 //number of likes after user liked post (+1)
+	// console.log("\"" + numoflikes_str + "\" -- \"" + new_numoflikes_str + "\"");
+
+	var post = document.getElementById(div_id); // the div that will be set
+	
+	html_str = "<h6 id=numoflikes_" + message_id + ">Likes: " + new_numoflikes_str.toString() + "</h6>";
+	const onclick_unlike = " onclick=\"unlike_post('" + message_id + "','" + current_id + "')\"" 
+	html_str += "<br>";
+	html_str += "<button div=unlike_button_" + message_id; 
+	html_str += " type=\"button\"";
+	html_str += " class=\"btn btn-outline-danger\""
+	html_str += onclick_unlike;
+	html_str += ">";
+	html_str += "Unlike 💔"
+	html_str +=  "</button>"
+
+	post.innerHTML = `${html_str}`;
+}
+
+function reset_like_div(message_id, current_id){ // pressing the unlike button creates the like button
+	// console.log("resetting the div for: " + message_id.toString())
+	const div_id = "like_div_" + message_id.toString() // id for the edit div
+	const numoflikes_id = "numoflikes_" + message_id.toString() // id for the "Likes" label
+
+	const numoflikes_str = document.getElementById(numoflikes_id).innerText.charAt(7); //number of likes before user liked post
+	var new_numoflikes_str = parseInt(numoflikes_str) - 1 //number of likes after user liked post (-1)
+	// console.log("\"" + numoflikes_str + "\" -- \"" + new_numoflikes_str + "\"");
+
+	var post = document.getElementById(div_id); // the div that will be set
+	html_str = "<h6 id=numoflikes_" + message_id + ">Likes: " + new_numoflikes_str.toString() + "</h6>";
+	const onclick_like = " onclick=\"like_post('" + message_id + "','" + current_id + "')\"" 
+	html_str += "<br>";
+	html_str += "<button div=like_button_" + message_id;
+	html_str += " type=\"button\"";
+	html_str += " class=\"btn btn-outline-primary\""
+	html_str += onclick_like;
+	html_str += ">";
+	html_str += "Like 💗"
+	html_str +=  "</button>"
+
+	post.innerHTML = `${html_str}`;	
 }
 
 function edit_post(message_id, init_content){
